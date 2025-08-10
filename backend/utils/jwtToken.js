f65dc6a2398b1a82609bb88f2d4a0970373a9633
Divesh Saini <diveshsaini1991@gmail.com>
2025-08-10 13:52:25 +0530

    fix : fixed footer animation issue and auth issue

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -3,8 +3,7 @@ export const generateToken = (user, message, statusCode, res) => {
     const cookieName = user.role === "Admin" ? "adminToken" : (user.role === "Doctor" ? "doctorToken" : "patientToken");
     res.status(statusCode).cookie(cookieName, token, {
         expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000), 
-        httpOnly: true,  
-        secure: true,  
+        httpOnly: true, 
         sameSite: 'None',  
         domain: process.env.BACKEND_DOMAIN,  
     })
